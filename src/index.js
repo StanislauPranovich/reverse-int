a8dc37d7364c45635dbfa644c110769d2a8597d8
Stanislau Pranovich <stanislau.pranovich@gmail.com>
2020-10-09 19:16:08 +0300

    feat: add task solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (num) {
+    num = Math.abs(num);
+    let result = 0;
+    while(num) {
+        result = (result*10) + num%10;
+        num = Math.floor(num/10);
+    }
+    return result;
 }
